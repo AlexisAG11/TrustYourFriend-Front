39cc1b4f505e504f867f37f3d03710cf766f7a8b
Alexis <pseudoalex@outlook.fr>
2024-10-31 20:48:18 +0900

    Blurry loader while sorting

--- src/app/feed/feed-center/feed-center.component.ts
+++ src/app/feed/feed-center/feed-center.component.ts
@@ -28,25 +28,72 @@ export class FeedCenterComponent implements OnInit,OnDestroy {
   @Input() idUser = "";
   @Input() types: any[] = [];
   @Input() adresses: any[] = [];
-  inFiltering: boolean = false
+  inFiltering: boolean = false;
+  allImageLoaded: boolean = true;
+  temporaryPlaces: any = [];
 
 
   constructor(private feedService: FeedService, private authService: AuthentificationService){}
 
   ngOnInit(): void {
     this.sub2 = this.feedService.placeSubject.subscribe( data => {
-      this.places = data.modifiedPlaces;
+      // to replace the data only when everything of the new data is loaded (images)
+      this.temporaryPlaces = data.modifiedPlaces;
+      // case if there isn't any data
+      if (this.temporaryPlaces.length === 0) {
+        this.allImageLoaded = true;
+        this.places = this.temporaryPlaces;
+      }
+      else {
+        // preload images
+        this.checkImages();
+      }
     })
 
     this.feedService.deletePlaceSubject.subscribe((id) => {
       this.places = this.places.filter((ele:any) => ele._id != id);
+
     })
 
     this.feedService.inFilteringSubject.subscribe( data => {
       this.inFiltering = data;
+      this.allImageLoaded = false;
+
     })
 
   }
+
+  checkImages() {
+    // Get all image URLs from the new data (adjust based on your data structure)
+    const imageUrls = this.temporaryPlaces.map((item: any) => item.imageUrl);
+    const totalImages = imageUrls.length;
+    let loadedImages = 0;
+
+    // Create an Image element for each image URL to preload them
+    imageUrls.forEach((url:any) => {
+      const img = new Image();
+      
+      img.onload = () => {
+        loadedImages++;
+        // Check if all images are loaded
+        if (loadedImages === totalImages) {
+          this.allImageLoaded = true;
+          this.places = this.temporaryPlaces;
+        }
+      };
+      
+      img.onerror = () => {
+        loadedImages++;
+        // Check if all images are loaded (consider errors too)
+        if (loadedImages === totalImages) {
+          this.allImageLoaded = true;
+          this.places = this.temporaryPlaces;
+        }
+      };
+      img.src = url; // Start loading the image
+    });
+  }
+
 
 
 
